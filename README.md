e3efea31dc06581be05d47c18e7f984a59300b53
Brenton Horne <brentonhorne77@gmail.com>
2025-10-13 22:42:58 +1000

    Adding README, install.sh and hyprpaper.conf

---
+++ README.md
@@ -0,0 +1,9 @@
+# My Hyprland configuration files
+![](https://fusion809.github.io/images/Hyprland/Screenshot_2025-10-13_22-23-17.png)
+
+**Figure 1: Hyprland configuration as of 13 October 2025.**
+
+This repository contains my Hyprland configuration files. 
+
+## Installation
+To install these scripts, run the install.sh script. It merely gets the repo and sets up appropriate symlinks. 
---
+++ install.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+if ! [[ -d $HOME/GitHub/mine/config/hyprland-configs ]]; then
+	pushd $HOME/GitHub/mine/config
+	git clone https://github.com/fusion809/hyprland-configs
+	popd
+fi
+pushd $HOME/GitHub/mine/config/hyprland-configs
+if ! [[ -d $HOME/.local/bin ]]; then
+	mkdir -p $HOME/.local/bin
+fi
+ln -sf $PWD/bluetooth-connect ~/.local/bin/
+ln -sf $PWD/focus-chrome ~/.local/bin/
+ln -sf $PWD/focus-kitty ~/.local/bin/
+ln -sf $PWD/focus-nautilus ~/.local/bin/
+ln -sf $PWD/runescape ~/.local/bin/
+ln -sf $PWD/wallpaper ~/.local/bin/
+if ! [[ -d $HOME/.config/waybar ]]; then
+	mkdir -p $HOME/.config/waybar
+fi
+ln -sf $PWD/arch_menu.xml ~/.config/waybar/
+ln -sf $PWD/power_menu.xml ~/.config/waybar/
+ln -sf $PWD/config.jsonc ~/.config/waybar/
+ln -sf $PWD/style.css ~/.config/waybar/
+if ! [[ -d $HOME/.config/hypr ]]; then
+	mkdir -p $HOME/.config/hypr
+fi
+ln -sf $PWD/hyprland.conf ~/.config/hypr/
+if ! [[ -d $HOME/.config/kitty ]]; then
+	mkdir -p $HOME/.config/kitty
+fi
+ln -sf $PWD/kitty.conf ~/.config/kitty/
+popd
