de623982f50822b897a3b7d351671ae2bdf3c44f
Brenton Horne <brentonhorne77@gmail.com>
2025-10-30 01:45:58 +1000

    Using pikaur may be necessary for updater to list AUR updates, too

--- install.sh
+++ install.sh
@@ -5,6 +5,6 @@ if ! [[ -d $HOME/GitHub/mine/config/hyprland-configs ]]; then
 	popd
 fi
 ./setup-symlinks.sh
-yay -S alacritty kitty hyprland waybar bluetoothctl nautilus google-chrome steam rofi swaybg gvim-gtk2 wttrbar wl-clipboard cmake meson cpio pkg-config git blueman-manager marked hyfetch gtop clipman --noconfirm --needed
+yay -S alacritty kitty hyprland waybar bluetoothctl nautilus google-chrome steam rofi swaybg gvim-gtk2 wttrbar wl-clipboard cmake meson cpio pkg-config git blueman-manager marked hyfetch gtop clipman pikaur --noconfirm --needed
 hyprpm add https://github.com/outfoxxed/hy3
 hyprpm update
